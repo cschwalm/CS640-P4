9999c444ff70f3d7f73cfeec630b11d61745b91c
Corbin Schwalm <corbin.schwalm@twinlakesprogramming.com>
2015-04-19 21:28:10 -0500

    Started DNS Forwarding Logic

--- src/edu/wisc/cs/sdn/simpledns/Server.java
+++ src/edu/wisc/cs/sdn/simpledns/Server.java
@@ -13,7 +13,8 @@ import edu.wisc.cs.sdn.simpledns.packet.DNS;
  */
 public class Server {
 
-	private static final int DNS_PORT = 8053;
+	private static final int CUSTOM_DNS_PORT = 8053;
+	private static final int DNS_PORT = 53;
 	private static final int BYTEBUFFER_SIZE = 28;
 
 	public Server() {
@@ -33,7 +34,7 @@ public class Server {
 
 		try {
 
-			DatagramSocket serverSocket = new DatagramSocket(DNS_PORT);
+			DatagramSocket serverSocket = new DatagramSocket(CUSTOM_DNS_PORT);
 
 			DatagramPacket data = new DatagramPacket(buffer, buffer.length);
 			serverSocket.receive(data);
@@ -42,6 +43,45 @@ public class Server {
 			
 			serverSocket.close();
 
+		} catch (IOException ex) {
+			System.out.println("IO Error Occurred. Exiting...");
+			System.exit(1);
+		}
+		
+		return dns;
+	}
+	
+	/**
+	 * Forwards the specified request to the specified name server.
+	 * The response from the DNS server is returned.
+	 * 
+	 * @param request
+	 * @return The DNS response.
+	 */
+	protected DNS forwardRequest(DNS request, String ip) {
+		
+		byte[] buffer = new byte[BYTEBUFFER_SIZE];
+		DNS dns = null;
+		
+		try {
+			
+			InetAddress inet = InetAddress.getByName(ip);
+
+			/* Send Request */
+			DatagramSocket serverSocket = new DatagramSocket();
+			DatagramPacket send = new DatagramPacket(request.serialize(), 
+					request.getLength(), inet, DNS_PORT);
+			
+			serverSocket.send(send);
+			
+			/* Receive Response */
+			DatagramPacket receive = new DatagramPacket(buffer, buffer.length);
+			serverSocket.receive(receive);
+			buffer = receive.getData();
+			dns = DNS.deserialize(buffer, receive.getLength());
+			
+			serverSocket.close();
+			
 		} catch (IOException ex) {
 			System.out.println("IO Error Occurred. Exiting...");
 			System.exit(1);
--- src/edu/wisc/cs/sdn/simpledns/SimpleDNS.java
+++ src/edu/wisc/cs/sdn/simpledns/SimpleDNS.java
@@ -1,6 +1,7 @@
 package edu.wisc.cs.sdn.simpledns;
 
 import edu.wisc.cs.sdn.simpledns.packet.DNS;
+import edu.wisc.cs.sdn.simpledns.packet.DNSQuestion;
 
 /**
  * Creates a simple server that processes DNS requests
@@ -10,10 +11,32 @@ import edu.wisc.cs.sdn.simpledns.packet.DNS;
 public class SimpleDNS {
 	
 	public static Server server;
+	public final static String ROOT_NS = "198.41.0.4";
 	
 	public static void main(String[] args) {
 		
 		server = new Server();
-		DNS dns = server.processDNSRequest();
+		
+		while (true) {
+			
+			DNS dns = server.processDNSRequest();
+			byte opcode = dns.getOpcode();
+			if (dns.getQuestions().size() <= 0) {
+				continue;
+			}
+			
+			DNSQuestion question = dns.getQuestions().get(0);
+			
+			/* Skip Invalid Record Types */
+			if (opcode != DNS.OPCODE_STANDARD_QUERY && 
+					question.getType() != DNS.TYPE_A && question.getType() != DNS.TYPE_AAAA &&
+					question.getType() != DNS.TYPE_NS && question.getType() != DNS.TYPE_CNAME) { 
+				continue;
+			}
+			
+			/* Get Response From Root NS */
+			DNS nsResponse = server.forwardRequest(dns, ROOT_NS);
+			System.out.println(nsResponse);
+		}
 	}
 }
